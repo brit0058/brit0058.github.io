6a6dbe3f08b74098a2f2065d2374ee7e3f481dac
brit0058 <77294254+brit0058@users.noreply.github.com>
2021-04-19 18:01:52 -0400

    Update form_validation.js

--- lib2024/final/js/form_validation.js
+++ lib2024/final/js/form_validation.js
@@ -3,19 +3,26 @@ window.onload = init;
 function init() {
 
 	$('#form').submit(function (e) {
-		e.preventDefault();
-		$("#results").removeClass('reveal');
-		var form = this;
-	})
+   		 e.preventDefault();
+   		 $("#results").removeClass('reveal');
+   		 var form = this;
 
-});
+	});
 
 }
 
-function showformValues(form) {
-	var formValues = $(form).serializeArray();
-	$.each(formValues, function(index,field){
-		$("#results").find("#"+field.name+"result").attr("href", "mailto:"+field.value);
+function showformValues(form){
+
+	var formValues = $(form).serializeArray(); 
+
+		
+	$.each(formValues, function(index, field){
+
+		$("#results").find("#"+field.name+"_result").text(field.value);
+
+
+		if(field.name=="email"){
+			$("#results").find("#"+field.name+"_result").attr("href", "mailto:"+field.value);
 		}
-	})
+	})				
 }
